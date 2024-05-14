30ab3124ede39db47ee2eb6dea0f5e160893b5c3
vctrhugoop <vhdestroy@hotmail.com>
2024-05-14 19:59:53 -0300

    ✨ feat: add in components skeleton for loading state

--- src/pages/app/dashboard/day-orders-amount-card.tsx
+++ src/pages/app/dashboard/day-orders-amount-card.tsx
@@ -4,6 +4,8 @@ import { ReceiptText } from 'lucide-react'
 import { getDayOrdersAmount } from '@/api/get-day-orders-amount'
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 
+import { MetricCardSkeleton } from './metric-card-skeleton'
+
 export function DayOrdersAmountCard() {
   const { data: dayOrdersAmount } = useQuery({
     queryFn: getDayOrdersAmount,
@@ -17,7 +19,7 @@ export function DayOrdersAmountCard() {
         <ReceiptText className="size-4 text-muted-foreground" />
       </CardHeader>
       <CardContent className="space-y-1">
-        {dayOrdersAmount && (
+        {dayOrdersAmount ? (
           <>
             <span className="text-2xl font-bold tracking-tight">
               {dayOrdersAmount.amount.toLocaleString('pt-BR')}
@@ -35,6 +37,8 @@ export function DayOrdersAmountCard() {
               em relação a ontem
             </p>
           </>
+        ) : (
+          <MetricCardSkeleton />
         )}
       </CardContent>
     </Card>
