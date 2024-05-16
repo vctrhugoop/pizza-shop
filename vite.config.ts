933ee5c6600879b60516630ae7e5b0420c779e58
vctrhugoop <vhdestroy@hotmail.com>
2024-05-16 19:23:30 -0300

    🧪 test: setup test

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import path from 'node:path'
 
 import react from '@vitejs/plugin-react'
-import { defineConfig, UserConfig } from 'vite'
+import type { UserConfig } from 'vite'
+import { defineConfig } from 'vite'
 import type { InlineConfig } from 'vitest'
 
 export default defineConfig({
@@ -13,6 +14,8 @@ export default defineConfig({
   },
   test: {
     globals: true,
+    setupFiles: ['./test/setup.ts'],
+    environment: 'happy-dom',
   },
 } as UserConfig & {
   test: InlineConfig
